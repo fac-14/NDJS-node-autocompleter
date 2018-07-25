49fcab1345a0b42cd1998d86397ad3da484e35bb
Dominic Coelho <domwork@live.com>
2018-07-25 15:40:45 +0100

    XHR generic function setup

--- public/script.js
+++ public/script.js
@@ -5,3 +5,17 @@ searchInput.addEventListener('keyup', function (e){
     console.log(searchInput.value);
     }
 });
+
+function genericXHR (url, cb) {
+    var xhr = new XMLHttpRequest(url, cb);
+    xhr.onreadystatechange = function () {
+        if(xhr.readyState === 4 && xhr.status === 200) {
+            var parsedObj = JSON.parse(xhr.responseText);
+            return cb(parsedObj);
+        } else if (xhr.readyState === 4 && xhr.status != 200) {
+            console.log("sorry XHR unavailable");
+        }
+    }
+    xhr.open("GET", url, true);
+    xhr.send();
+}
