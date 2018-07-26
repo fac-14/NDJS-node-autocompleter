ab3f945365642f29a4d92cc89fcc144a0b7da58f
Jessie Beech <jessie@jessiebeech.com>
2018-07-26 10:22:54 +0100

    add JSON file and formatting src files Relates #5 #6

--- src/tests.js
+++ src/tests.js
@@ -1,22 +1,23 @@
-const test = require('tape');
-const handlers = require('./handlers');
-const routes = require('./routes')
+const test = require("tape");
+const handlers = require("./handlers");
+const routes = require("./routes");
 
 // test('Testing Tape is working', function(t) {
 //   t.equal(1, 1, 'One should equal one');
 //   t.end();
 // });
 
-
 test('testing that GET "/" returns statusCode 200', function(t) {
-   t.plan(2);
-   routes(
+  t.plan(2);
+  routes(
     { url: "/" },
-    {writeHead: (status, _content) => {
-      t.equal(status, 200)
-    },
-    end: (body) => {
-      t.ok("view = 'fac'"? body.includes("view = 'fac'") : body)
+    {
+      writeHead: (status, _content) => {
+        t.equal(status, 200);
+      },
+      end: body => {
+        t.ok("view = 'fac'" ? body.includes("view = 'fac'") : body);
+      }
     }
-    });
+  );
 });
